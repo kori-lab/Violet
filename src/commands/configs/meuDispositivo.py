2c75d506764da9bd791fcbd148b73ef111ef8cdb
akirou-lab <prxffadeus@gmail.com>
2021-08-16 12:57:17 -0300

    clear code

--- src/commands/configs/meuDispositivo.py
+++ src/commands/configs/meuDispositivo.py
@@ -1,17 +1,11 @@
 from src.utils.functions.selfInput import *;
 from src.utils.functions.clear import *;
 import os;
-import psutil;
 
 pid = os.getpid()
-py = psutil.Process(pid)
 
 message = f'''
             nome de usuario: {os.getlogin()}
-            memoria usada: {py.memory_info()[0]/2.**30}
-            psycal memoria use: {psutil.virtual_memory()}
-            net stats: {psutil.net_if_stats()}
-            rede: {psutil.net_io_counters()}
         '''
 def run():
     Sair = False;
